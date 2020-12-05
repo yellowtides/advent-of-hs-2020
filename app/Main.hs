2c543ad4b341ca2873327b77393dd5f457bf515f
Andi <s2095623@ed.ac.uk>
2020-12-05 09:47:39 +0000

    Day 5 done

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import qualified Day1 as D1 (getSols)
 import qualified Day2 as D2 (getSols)
 import qualified Day3 as D3 (getSols)
 import qualified Day4 as D4 (getSols)
+import qualified Day5 as D5 (getSols)
 
 import Utils (parseConstraint, isBetween)
 
@@ -34,6 +35,8 @@ main = do
                              splitOn "\n" file2)
             4 -> D4.getSols (splitOn "\n\n" file1, 
                              splitOn "\n\n" file2)
+            5 -> D5.getSols (splitOn "\n" file1, 
+                             splitOn "\n" file2)
             _ -> error "Patience, fren."
     putStrLn $ "Star #1: " ++ star1
     putStrLn $ "Star #2: " ++ star2
---
+++ src/Day5.hs
@@ -0,0 +1,40 @@
+module Day5 where
+
+import Data.Bifunctor (first)
+import Data.Sort (sort)
+
+{-
+    [[row ID binary rep (127 complement)]]
+    F -> 1
+    B -> 0
+    
+    [[column ID binary rep (7 complement)]]
+    R -> 0
+    L -> 1
+-}
+readPassC :: String -> Int
+readPassC = foldl1 ((+) . (* 2)) . map (\x -> case x of {'F' -> 1; 'B' -> 0;
+                                                         'L' -> 1; 'R' -> 0})
+
+passToLoc :: String -> (Int, Int) -- (row, column)
+passToLoc s = let (rowStr, colStr) = break (`elem` "LR") s in
+                (127 - readPassC rowStr,
+                   7 - readPassC colStr)
+
+getID :: String -> Int
+getID = uncurry (+) . first (8 *) . passToLoc
+
+getLowestID :: [String] -> Int
+getLowestID = maximum . map getID
+
+getHighestID :: [String] -> Int
+getHighestID = maximum . map getID
+
+findMissing :: [String] -> Int
+findMissing ss = let sortedIDs = sort $ map getID ss in
+                    sum [head sortedIDs..last sortedIDs] 
+                    - sum sortedIDs
+
+getSols :: ([String], [String]) -> (String, String)
+getSols (inp1, inp2) = (show $ getHighestID inp1, 
+                        show $ findMissing inp2)
