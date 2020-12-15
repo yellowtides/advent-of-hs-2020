91ec242f577ccd220fdafbc54b7055160e4e6dbe
Andi <s2095623@ed.ac.uk>
2020-12-15 15:07:06 +0000

    Day 15 done

--- src/Day15.hs
+++ src/Day15.hs
@@ -5,15 +5,16 @@ import Data.Maybe (fromMaybe)
 
 -- Star #1
 
-playGame' :: Int -> Int -> HM.HashMap Int Int -> [Int]
-playGame' n pos mem = n : playGame' (pos - fromMaybe pos (HM.lookup n mem)) (pos+1) (HM.insert n pos mem)
+playGame' :: Int -> Int -> HM.HashMap Int Int -> Int -> Int
+playGame' n pos mem goal | pos == goal = n
+                         | otherwise   = playGame' (pos - fromMaybe pos (HM.lookup n mem)) (pos+1) (HM.insert n pos mem) goal
 
--- play the elves' game, _forever_!
-playGame :: [Int] -> [Int]
-playGame seed = init seed ++ playGame' (last seed) (length seed) (HM.fromList $ zip (init seed) [1..]) 
+-- play the elves' game til the goal is hit!
+playGame :: [Int] -> Int -> Int
+playGame seed = playGame' (last seed) (length seed) (HM.fromList $ zip (init seed) [1..])
 
 -- Star #2
 
 getSols :: ([Int], [Int]) -> (String, String)
-getSols (inp1, inp2) = (show $ playGame inp1!!(2020-1),
-                        show $ playGame inp2!!(3000000-1))
+getSols (inp1, inp2) = (show $ playGame inp1 2020,
+                        show $ playGame inp2 30000000)
