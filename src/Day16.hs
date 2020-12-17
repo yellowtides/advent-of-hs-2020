1ad8c1bc96f015c29b581b309800c8dcfd7b1890
Andi <s2095623@ed.ac.uk>
2020-12-17 01:18:01 +0000

    Prettify D16 solution

--- src/Day16.hs
+++ src/Day16.hs
@@ -13,7 +13,7 @@ isPossValid :: (LocRange, LocRange) -> Int -> Bool
 isPossValid (r1, r2) = isBetween r1 |:| isBetween r2 
 
 isPossValidAll :: [(LocRange, LocRange)] -> Int -> Bool
-isPossValidAll rules x = foldl (\acc r -> isPossValid r x || acc) False rules
+isPossValidAll rules x = or $ (`isPossValid` x) <$> rules
 
 scanError :: [(LocRange, LocRange)] -> [Int] -> Int
 scanError rules = sum . filter (not . isPossValidAll rules) 
@@ -21,22 +21,24 @@ scanError rules = sum . filter (not . isPossValidAll rules)
 -- Star #2
 
 pruneOptions :: HM.HashMap Int [Int] -> [(LocRange, LocRange)] -> (Int, Int) -> HM.HashMap Int [Int]
-pruneOptions cand rules (pos, x)
-    = foldl (\mem (pos', i') -> HM.adjust (delete i') pos mem) cand 
-      $ [(pos, i) | (i, r) <- zip [0..] rules, not $ isPossValid r x]
+pruneOptions cand rules (pos, x) = foldr (\i' -> HM.adjust (delete i') pos) cand 
+                                   $ [i | (i, r) <- zip [0..] rules, not $ isPossValid r x]
 
 decipher :: HM.HashMap Int [Int] -> [(LocRange, LocRange)] -> [(Int, Int)] -> HM.HashMap Int [Int]
-decipher mem rules pxs = foldl (\mem px -> pruneOptions mem rules px) mem pxs
+decipher mem rules = foldl (`pruneOptions` rules) mem
 
 findPairs :: [(Int, [Int])] -> [(Int, Int)]
-findPairs []                           = []
-findPairs ((f, (wtfisthiscode:es)):ps) = (f, wtfisthiscode) : findPairs (map (fmap (delete wtfisthiscode)) ps)
+findPairs []             = []
+findPairs ((f, x:es):ps) = (f, x) : findPairs (map (fmap $ delete x) ps)
 
 getSols :: (([(LocRange, LocRange)], Ticket, [Ticket]), 
             ([(LocRange, LocRange)], Ticket, [Ticket])) -> (String, String)
-getSols (inp1, inp2) = (show $ scanError (fst3 inp1) (concat $ thd3 inp1),
-                        show . product . map (\(a, _) -> snd3 inp2!!a) . take 6 . sortOn (\(_, b) -> b) .
-                               findPairs . sortOn (\(_, xs) -> length xs) . HM.toList $
-                               decipher (HM.fromList $ zipWith (,) [0..length (snd3 inp2) - 1] (repeat [0..length (snd3 inp2) - 1]))
-                                        (fst3 inp2)
-                                        (concat . map (zip [0..]) $ filter (all (isPossValidAll (fst3 inp2))) (thd3 inp2)))
+getSols (inp1, inp2) = (show $ scanError rules1 (concat othTickets1),
+                        show . product . map ((!!) myTicket2 . fst) . take 6 . sortOn snd .
+                               findPairs . sortOn (length . snd) . HM.toList $
+                               decipher (HM.fromList $ zip initPossib (repeat initPossib)) rules2
+                                        (concatMap (zip [0..]) $ filter (all $ isPossValidAll rules2) othTickets2))
+                    where
+                      (rules1, myTicket1, othTickets1) = inp1
+                      (rules2, myTicket2, othTickets2) = inp2
+                      initPossib                       = [0..length myTicket2 - 1]
